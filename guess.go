6685a4b865c21b319dc40f0bcbad657f4c154cd0
thisisnew <thisisnew@naver.com>
2021-04-01 19:35:38 +0900

    study headfirst go chapter02 finished

--- guess.go
+++ guess.go
@@ -17,18 +17,35 @@ func main() {
 	target := rand.Intn(100) + 1
 	fmt.Println("I've chosen a random number between 1 and 100")
 	fmt.Println("Can you guess it?")
-	fmt.Println(target)
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Make a guess:")
-	input, err := reader.ReadSlice('\n')
-	if err != nil {
-		log.Fatal(err)
+	success := false
+
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Print("Make a guess:")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			success = true
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
 	}
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
+
+	if !success {
+		fmt.Println("Sorry you didn't guess my number. It was: ", target)
 	}
 
 }
