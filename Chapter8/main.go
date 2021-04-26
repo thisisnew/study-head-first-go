9b37b0c979a37ce75995cb6117840b59de02c4e4
thisisnew <thisisnew@naver.com>
2021-04-26 14:00:59 +0900

    구조체

--- Chapter8/main.go
+++ Chapter8/main.go
@@ -5,33 +5,15 @@ import (
 	magazine "study-head-first-go/Chapter8/magazine"
 )
 
-type myStruct struct {
-	myField int
-}
-
-func printInfo(s *magazine.Subscriber) {
-	fmt.Println("Name:", s.Name)
-	fmt.Println("Monthly rate:", s.Rate)
-	fmt.Println("Active:", s.Active)
-}
-
-func defaultSubscriber(name string) *magazine.Subscriber {
-	var s magazine.Subscriber
-	s.Name = name
-	s.Rate = 5.99
-	s.Active = true
-	return &s
-}
-
-func applyDiscount(s *magazine.Subscriber) {
-	s.Rate = 4.99
-}
-
 func main() {
-	subscriber1 := defaultSubscriber("Aman Singh")
-	applyDiscount(subscriber1)
-	printInfo(subscriber1)
+	address := magazine.Address{
+		Street:     "123 Oak St",
+		City:       "Omaha",
+		State:      "NE",
+		PostalCode: "6811",
+	}
 
-	subscriber2 := defaultSubscriber("Beth Ryan")
-	printInfo(subscriber2)
+	subscriber := magazine.Subscriber{Name: "Aman Singh"}
+	subscriber.HomeAddress = address
+	fmt.Println(subscriber.HomeAddress)
 }
