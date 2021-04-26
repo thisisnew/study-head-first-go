154c862016965c87f474f4a9700d6a74951f2716
thisisnew <thisisnew@naver.com>
2021-04-26 10:47:06 +0900

    슬라이스

---
+++ Chapter6/average/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"study-head-first-go/Chapter6/datafile"
+)
+
+func main() {
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+}
