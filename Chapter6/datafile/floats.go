61a2b8a0cffa390523869080950aa8318c41f9d8
thisisnew <thisisnew@naver.com>
2021-04-26 11:11:08 +0900

    슬라이스

--- Chapter6/datafile/floats.go
+++ Chapter6/datafile/floats.go
@@ -10,24 +10,24 @@ func GetFloats(fileName string) ([]float64, error) {
 	var numbers []float64
 	file, err := os.Open(fileName)
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
 		numbers = append(numbers, number)
 	}
 	err = file.Close()
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 
 	if scanner.Err() != nil {
-		return numbers, scanner.Err()
+		return nil, scanner.Err()
 	}
 
 	return numbers, nil
