8ea4e064c48fcaaececc8f5cbd46ea2790ead554
thisisnew <thisisnew@naver.com>
2021-04-26 12:41:30 +0900

    맵

--- Chapter7/practice/main.go
+++ Chapter7/practice/main.go
@@ -3,13 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	jewerly := make(map[string]float64)
-	jewerly["necklace"] = 89.99
-	jewerly["earrings"] = 79.99
 
-	clothing := map[string]float64{"pants": 59.99, "shirt": 39.99}
-	fmt.Println("Earrings:", jewerly["earrings"])
-	fmt.Println("Necklace:", jewerly["necklace"])
-	fmt.Println("Shirt:", clothing["shirt"])
-	fmt.Println("Pants:", clothing["pants"])
+	data := []string{"a", "c", "e", "a", "e"}
+	counts := make(map[string]int)
+
+	for _, item := range data {
+		counts[item]++
+	}
+
+	letters := []string{"a", "b", "c", "d", "e"}
+
+	for _, letter := range letters {
+		count, ok := counts[letter]
+
+		if !ok {
+			fmt.Printf("%s: not found\n", letter)
+		} else {
+			fmt.Printf("%s: %d\n", letter, count)
+		}
+
+	}
 }
