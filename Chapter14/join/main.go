92ad3a77534b84160bdfc38e8275cc6c4e559e65
thisisnew <thisisnew@naver.com>
2021-04-27 11:30:15 +0900

    고루틴과 채널

---
+++ Chapter14/join/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"study-head-first-go/Chapter14/prose"
+)
+
+func main() {
+	phrases := []string{"my parents", "a rodeo clown"}
+	fmt.Println("A photo of", prose.JoinWithCommas(phrases))
+	phrases = []string{"my parents", "a rodeo clown", "a prize bull"}
+	fmt.Println("A photo of", prose.JoinWithCommas(phrases))
+}
