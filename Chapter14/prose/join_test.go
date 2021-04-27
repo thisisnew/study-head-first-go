110afa6b99265463c75f11508cf780fff4e33bb0
thisisnew <thisisnew@naver.com>
2021-04-27 13:21:33 +0900

    자동테스트

--- Chapter14/prose/join_test.go
+++ Chapter14/prose/join_test.go
@@ -5,6 +5,37 @@ import (
 	"testing"
 )
 
+type testData struct {
+	list []string
+	want string
+}
+
+func TestJoinWithCommas(t *testing.T) {
+	tests := []testData{
+		testData{
+			list: []string{"apple"},
+			want: "apple",
+		},
+		testData{
+			list: []string{"apple", "orange"},
+			want: "apple and orange",
+		},
+		testData{
+			list: []string{"apple", "orange", "pear"},
+			want: "apple, orange, and pear",
+		},
+	}
+
+	for _, test := range tests {
+		list := test.list
+		want := test.want
+		got := JoinWithCommas(list)
+		if got != want {
+			t.Error(errorString(list, got, want))
+		}
+	}
+}
+
 func TestOneElement(t *testing.T) {
 	list := []string{"apple"}
 	want := "apple"
