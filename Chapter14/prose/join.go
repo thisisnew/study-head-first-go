dd3333a7967c513573588a23f24b298571ee01e3
thisisnew <thisisnew@naver.com>
2021-04-27 13:15:08 +0900

    자동테스트

--- Chapter14/prose/join.go
+++ Chapter14/prose/join.go
@@ -3,8 +3,14 @@ package prose
 import "strings"
 
 func JoinWithCommas(phrase []string) string {
-	result := strings.Join(phrase[:len(phrase)-1], ", ")
-	result += ", and "
-	result += phrase[len(phrase)-1]
-	return result
+	if len(phrase) == 1 {
+		return phrase[0]
+	} else if len(phrase) == 2 {
+		return phrase[0] + " and " + phrase[1]
+	} else {
+		result := strings.Join(phrase[:len(phrase)-1], ", ")
+		result += ", and "
+		result += phrase[len(phrase)-1]
+		return result
+	}
 }
