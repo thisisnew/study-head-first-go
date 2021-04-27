b56f7a391b491b7f813854f9ccf5aaa806d06c24
thisisnew <thisisnew@naver.com>
2021-04-27 14:20:14 +0900

    HTML 템플릿

--- Chapter16/myproject/guestbook.go
+++ Chapter16/myproject/guestbook.go
@@ -19,8 +19,23 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	check(err)
 }
 
+func newHanlder(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("new.html")
+	check(err)
+	err = html.Execute(w, nil)
+	check(err)
+}
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+	signature := r.FormValue("signature")
+	_, err := w.Write([]byte(signature))
+	check(err)
+}
+
 func main() {
 	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHanlder)
+	http.HandleFunc("/guestbook/create", createHandler)
 	err := http.ListenAndServe(":8000", nil)
 	log.Fatal(err)
 }
